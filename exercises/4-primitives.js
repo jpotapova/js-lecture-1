7b722845817436234fb538d20243c4147fc84f80
Jelizaveta Potapova <jelizaveta.potapova@devbridge.com>
2019-11-18 16:21:13 +0200

    Add exercise for primitives

---
+++ exercises/4-primitives.js
@@ -0,0 +1,22 @@
+/*
+  task 1
+  result1 should contain input1 in all capital letters
+*/
+var input1 = "abcdef";
+var result1;
+
+/*
+  task 2
+  result2 should contain an answer
+  whether date1 is later than date2
+*/
+var date1 = "2020-11-23";
+var date2 = "2020-03-11";
+var result2;
+
+// ignore the code below
+// it is out of the scope of this lecture
+module.exports = {
+  result1: result1,
+  result2: result2
+};
